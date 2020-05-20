1720de295ef07150f4c788a2b2abc03340acb35e
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-05-20 17:29:41 +0100

    added build pack

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 import React from "react";
 import Sidebar from "./component/Sidebar/Sidebar";
-import {SidebarItemProvider} from "./component/Sidebar/SidebarContext.js"
+import { SidebarItemProvider } from "./component/Sidebar/SidebarContext.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Dashboard";
-import Youth from "./Dashboard/Youth"
-import NotFound from "./component/NotFound"
+import Youth from "./Dashboard/Youth";
+import NotFound from "./component/NotFound";
 import ApolloClient from "apollo-boost";
-import {ApolloProvider} from "react-apollo"
+import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
-    uri:"https://hybackend.herokuapp.com/graphql"
-})
+  uri: "https://hybackend.herokuapp.com/graphql",
+});
 function App() {
   return (
-  <ApolloProvider client={client}>
-<div style={{overflow:"hidden", display:"flex",background:"#F3F4F7",height:"100%"}}>
-  <Router>
-    <SidebarItemProvider> 
-        <Sidebar />
-    </SidebarItemProvider>
-    
-    <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/Youths" component={Youth} />
-     <Route component={NotFound} />
-    </Switch>
-    </Router>
-  </div>
-  </ApolloProvider>
+    <ApolloProvider client={client}>
+      <div
+        style={{
+          overflow: "hidden",
+          display: "flex",
+          background: "#F3F4F7",
+          height: "100%",
+        }}
+      >
+        <Router>
+          <SidebarItemProvider>
+            <Sidebar />
+          </SidebarItemProvider>
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/Youths" component={Youth} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+      </div>
+    </ApolloProvider>
   );
 }
 
